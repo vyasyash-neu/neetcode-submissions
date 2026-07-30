ca685dcb9299c92beb0c64161948a63c637f449f
Yash Vyas <vyas.yash@northeastern.edu>
2026-07-30 00:50:57 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # # brute-force 2 pointer approach
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if nums[i] + nums[j] == target and i!=j:
+        #             return [i, j]
+        # # Space -> O(1)
+        # # time -> O(n^2)
+
+        # can we do it in O(n)?
+        # some_value = target - val 
+        hash_map = {} # key -> some_value
+                      # value -> index of 
+        for index, some_val in enumerate(nums):
+            if some_val in hash_map:
+                return [hash_map[some_val], index]
+            else:
+                some_val = target - some_val
+                hash_map[some_val] = index
